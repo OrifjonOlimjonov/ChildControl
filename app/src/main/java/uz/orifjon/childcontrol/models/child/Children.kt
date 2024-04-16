51919e5a34107c17ddae5bb281eec153c6315778
Orifjon <orifjon.olimjonov@mail.ru>
2024-04-16 07:40:49 +0500

    Children class fixed for Room

--- app/src/main/java/uz/orifjon/childcontrol/models/child/Children.kt
+++ app/src/main/java/uz/orifjon/childcontrol/models/child/Children.kt
@@ -7,10 +7,28 @@ import androidx.room.Relation
 @Entity
 class Children {
     @PrimaryKey(autoGenerate = true)
-    val childId: Long = 0
-    val locationNow: Pair<Double, Double>? = null
-    @Relation(parentColumn = "childId",
-        entityColumn = "taskId")
-    val taskList: List<Task>? = null
+    var childId: Long = 0
+    var locationNow: Pair<Double, Double>? = null
+
+    @Relation(
+        parentColumn = "childId",
+        entityColumn = "taskId"
+    )
+    var taskList: List<Task>? = null
+
+    constructor(childId: Long) {
+        this.childId = childId
+    }
+
+    constructor(childId: Long, locationNow: Pair<Double, Double>) {
+        this.childId = childId
+        this.locationNow = locationNow
+    }
+
+    constructor(childId: Long, locationNow: Pair<Double, Double>, taskList: List<Task>) {
+        this.childId = childId
+        this.locationNow = locationNow
+        this.taskList = taskList
+    }
 
 }
--- app/src/main/java/uz/orifjon/childcontrol/models/child/Task.kt
+++ app/src/main/java/uz/orifjon/childcontrol/models/child/Task.kt
@@ -1,18 +1,23 @@
 package uz.orifjon.childcontrol.models.child
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 
 @Entity
 class Task {
-
-    val taskId: Long = 0
+    @PrimaryKey(autoGenerate = true)
+    var taskId: Long = 0
     var title: String = ""
     var description: String = ""
     var deadline: String? = null
 
 
     constructor()
+
+    constructor(taskId: Long) {
+        this.taskId = taskId
+    }
 
     constructor(title: String) {
         this.title = title
